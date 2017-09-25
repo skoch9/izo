a5737fb2e34b15d52190be08f7dfbcbcbb904edd
JoKeR <dennis.loeben@gmx.net>
2017-09-25 23:53:27 +0200

    Time control with delay compensation Platy cannot fly anymore and has to walk everything

--- app/www/js/Game.js
+++ app/www/js/Game.js
@@ -4,117 +4,142 @@
 
 function Game() {
 
-	this.time = 0;     // [s]  Timestamp
-
-	/* Game screens */
-	this.gameScreen = new GameScreen;
-	
-	this.currentScreen = null;
-
-	this.changeScreen = function(screen) {
-		if(screen) {
-			if(this.currentScreen) {
-				this.currentScreen.unload(this);
-			}
-			this.currentScreen = screen;
-			this.currentScreen.load(this);
-		}
-	}
+    this.time       = 0;        // [s]  Timestamp of game
+    this.timeDelta  = 0;        // [s]  Difference between game time and external time
+    this.timeStep   = 1.0 / 60  // [s]  Time step of each update to achieve discrete clock
     
-	this.main = function(time) {
+    /* Game screens */
+    this.gameScreen = new GameScreen;
+    
+    this.currentScreen  = null;
+
+    // ThreeJS globals
+    var loader;
+    
+    this.changeScreen = function(screen) {
+        if(screen) {
+            if(this.currentScreen) {
+                this.currentScreen.unload(this);
+            }
+            this.currentScreen = screen;
+            this.currentScreen.load(this);
+        }
+    }
+    
+    this.main = function(time) {
         
         if(this.currentScreen) {
-            // Update screen
-            this.currentScreen.update(this);
+            
+            // Get the difference between external time and last game time
+            this.timeDelta  = (time / 1000.0) - this.time;        
+            
+            // Update the screen until synchronized with external time
+            var numUpdateCalls = 0;
+            
+            while(this.timeDelta > this.timeStep)
+            {
+                // Next time step
+                this.time       += this.timeStep;
+                                
+                // Update screen
+                this.currentScreen.update(this);
+                
+                this.timeDelta  -= this.timeStep;
+                
+                if(++numUpdateCalls > 20)
+                {
+                    console.log("Man, you've got serious performance issues (" + numUpdateCalls + " updates needed)!");
+                }
+            }
 
-            // Draw screen
+            // Draw screen synchronized with time
             this.currentScreen.draw(this);
         }
-        
-		this.time = time / 1000;   
-	}
+    }
 
-	this.init = function() {
+    this.init = function() {
         // Global game instance 
-		gameInstance = this;
+        gameInstance    = this;
         
-		this.changeScreen(this.gameScreen);
+        this.loader     = new THREE.JSONLoader();
+        
+        this.changeScreen(this.gameScreen);
         
         // Register event callbacks
         window.addEventListener('keydown',keyDownCallback,true);
-		window.addEventListener('keyup',keyUpCallback,true);
-		window.addEventListener('mousemove',mouseMoveCallback,true);
-		window.addEventListener('mousedown',mouseDownCallback,true);
-		window.addEventListener('mouseup',mouseUpCallback,true);
-	}	
-		
-	// Current key state
-	this.keyState  = new Array();    
+        window.addEventListener('keyup',keyUpCallback,true);
+        window.addEventListener('mousemove',mouseMoveCallback,true);
+        window.addEventListener('mousedown',mouseDownCallback,true);
+        window.addEventListener('mouseup',mouseUpCallback,true);
+    }	
+        
+    // Current key state
+    this.keyState  = new Array();    
     
-	/* Checks if given key is pressed */
-	this.isKeyPressed = function(key) {
-		if( key.Id in this.keyState && this.keyState[key.Id] ) {
-			return true;
-		} else {
-			return false;
-		}
-	}	    
+    /* Checks if given key is pressed */
+    this.isKeyPressed = function(key) {
+        if( key.Id in this.keyState && this.keyState[key.Id] ) {
+            return true;
+        } else {
+            return false;
+        }
+    }	    
     
     // Current mouse state
     this.mouseX     = -1;
     this.mouseY     = -1;
     
-	this.updateMousePosition = function(state) {        
-    	// calculate mouse position in normalized device coordinates
+    this.updateMousePosition = function(state) {        
+        // calculate mouse position in normalized device coordinates
         // (-1 to +1) for both components
         this.mouseX =   ( event.pageX / window.innerWidth  ) * 2 - 1;
         this.mouseY = - ( event.pageY / window.innerHeight ) * 2 + 1;
 
-	}    
+    }    
     
     /***********************************************
      * Global event callbacks
      ***********************************************/    
 
-	function mouseMoveCallback(state) {
-		gameInstance.updateMousePosition(state);
-	}
+    function mouseMoveCallback(state) {
+        gameInstance.updateMousePosition(state);
+    }
 
-	function mouseDownCallback(state) {
+    function mouseDownCallback(state) {
         gameInstance.updateMousePosition(state);
         
-		if(gameInstance.currentScreen.mouseDown) {
-			gameInstance.currentScreen.mouseDown(gameInstance.mouseX, gameInstance.mouseY);
-		}
-	}
+        if(gameInstance.currentScreen.mouseDown) {
+            gameInstance.currentScreen.mouseDown(gameInstance.mouseX, gameInstance.mouseY);
+        }
+    }
 
-	function mouseUpCallback(state) {
+    function mouseUpCallback(state) {
         gameInstance.updateMousePosition(state);
         
-		if(gameInstance.currentScreen.mouseUp) {
-			gameInstance.currentScreen.mouseUp(gameInstance.mouseX, gameInstance.mouseY);
-		}
-	}	    
+        if(gameInstance.currentScreen.mouseUp) {
+            gameInstance.currentScreen.mouseUp(gameInstance.mouseX, gameInstance.mouseY);
+        }
+    }	    
    
-	/* Set down key */
-	function keyDownCallback(state){
-		if(gameInstance.currentScreen.keyDown) {
-			gameInstance.currentScreen.keyDown(state.keyCode);
-		}	
-	
-		gameInstance.keyState[state.keyCode] = true;
-	}
-
-	/* Reset down key */
-	function keyUpCallback(state){
-		if(gameInstance.currentScreen.keyUp) {
-			gameInstance.currentScreen.keyUp(state.keyCode);
-		}
-	
-		gameInstance.keyState[state.keyCode] = false;
-	}    
+    /* Set down key */
+    function keyDownCallback(state){
+        if(gameInstance.currentScreen.keyDown) {
+            gameInstance.currentScreen.keyDown(state.keyCode);
+        }	
     
-	this.init();
+        gameInstance.keyState[state.keyCode] = true;
+    }
+
+    /* Reset down key */
+    function keyUpCallback(state){
+        if(gameInstance.currentScreen.keyUp) {
+            gameInstance.currentScreen.keyUp(state.keyCode);
+        }
+    
+        gameInstance.keyState[state.keyCode] = false;
+    }    
+    
+    this.init();
 }
 
 /* Global game instance */
--- app/www/js/GameScreen.js
+++ app/www/js/GameScreen.js
@@ -6,34 +6,32 @@ function GameScreen() {
 
 
 
-    var scene       = new THREE.Scene();
-    var camera      = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+    var scene           = new THREE.Scene();
+    var camera          = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
     camera.position.set(0, 10, 10);
 
-    var renderer    = new THREE.WebGLRenderer();
+    var renderer        = new THREE.WebGLRenderer();
 
-    var mixer = new THREE.AnimationMixer(scene);
-    var ambientLight = new THREE.AmbientLight(0xcccccc);
-    var pointLight = new THREE.PointLight(0xff4400, 5, 30);//ff4400
+    var mixer           = new THREE.AnimationMixer(scene);
+    var ambientLight    = new THREE.AmbientLight(0xcccccc);
+    var pointLight      = new THREE.PointLight(0xff4400, 5, 30);//ff4400
     var water;
     var platy;
-
-
-    var loader = new THREE.JSONLoader();
-    var raycaster   = new THREE.Raycaster();
+    var surface;
+    var raycaster       = new THREE.Raycaster(); // Move to game class
     
-    var targetPosition = new THREE.Vector3();
-    var targetDirection= new THREE.Vector3();
+    var targetPosition  = new THREE.Vector3();
+    var targetDirection = new THREE.Vector3();
     
     window.addEventListener('resize', function () 
     {
-      renderer.setSize(window.innerWidth, window.innerHeight);
-      camera.aspect = window.innerWidth / window.innerHeight;
-      camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
         
     });    
     
-    this.load = function() {
+    this.load = function(game) {
 
         renderer.setSize( window.innerWidth, window.innerHeight );
         document.body.appendChild( renderer.domElement );
@@ -47,31 +45,33 @@ function GameScreen() {
         // scene.fog = new THREE.FogExp2(0x000000, 0.035);
         // Setup camera
         // camera.position.z = 5;
-
-        loader.load('assets/platy.json', function (geometry, materials) {
+   
+        game.loader.load('assets/platy.json', function (geometry, materials) {
             platy = new THREE.Mesh(geometry, materials);
-            platy.position.set(-1, 3, 3);
+            platy.position.set(-1, 5, 3);
             // var s = THREE.Math.randFloat(0.00075, 0.001);
             var s = 0.5;
             platy.scale.set(s, s, s);
             //platy.rotation.y = THREE.Math.randFloat(-0.25, 0.25);
             //platy.matrixAutoUpdate = false;
-            platy.updateMatrix();
-            scene.add(platy);
-
+            
+            scene.add(platy);       
+            
+            
+       
         });
-        loader.load('assets/water.json', function (geometry, materials) {
+        game.loader.load('assets/water.json', function (geometry, materials) {
             water = new THREE.Mesh(geometry, materials);
             // mesh.position.set(3, 3, 3);
             scene.add(water);
         });
 
-        loader.load('assets/test2.json', function (geometry, materials) {
+        game.loader.load('assets/test2.json', function (geometry, materials) {
             // adjust color a bit
-            var mesh = new THREE.Mesh(geometry, materials);
-            mesh.position.set(0,0,0);
+            surface = new THREE.Mesh(geometry, materials);
+            surface.position.set(0,0,0);
             // console.log(geometry, materials)
-            scene.add(mesh);
+            scene.add(surface);
             // var material = materials[0];
             // material.morphTargets = true;
             // material.color.setHex(0xffaaaa);
@@ -90,17 +90,14 @@ function GameScreen() {
                 //     .startAt(-Math.random())	// random phase (already running)
                 //     .play();					// let's go
             // }
-        });
+        });        
+    };
 
-        // scene.add( cube );
-	};
+    this.unload = function(game) {
 
-	this.unload = function() {
-
-	};
-	
-	this.draw = function(game) {
-        // requestAnimationFrame(animate);
+    };
+    
+    this.draw = function(game) {
 
         camera.position.x = Math.cos(game.time / 5) * 10;
         camera.position.y = 8;
@@ -111,48 +108,56 @@ function GameScreen() {
 
         mixer.update(game.time);
         camera.lookAt(scene.position);
-        renderer.render(scene, camera);
-        // document.getElementById("hud").innerHTML = "t = " + t.toFixed(1) + " s";
+        renderer.render(scene, camera);      
+        
     };
-		
-	this.update = function(t) {
-
-		if ( game.isKeyPressed(Keys.LEFT) ){
-			water.position.y -= 0.01;
-		}
-		if ( game.isKeyPressed(Keys.RIGHT) ){
-			water.position.y += 0.01;
-		}
-
         
+    this.update = function(game) {
 
+        if ( game.isKeyPressed(Keys.LEFT) ){
+            water.position.y -= 0.01;
+        }
+        if ( game.isKeyPressed(Keys.RIGHT) ){
+            water.position.y += 0.01;
+        }
         
-        // Move to target position
-        if( platy.position.distanceTo( targetPosition ) > 0.1)
+        // Move platy to target position        
+        platyPosition2D     = new THREE.Vector2(platy.position.x, platy.position.z);    // Ignore y-axis for distance check
+        targetPosition2D    = new THREE.Vector2(targetPosition.x, targetPosition.z);    // Ignore y-axis for distance check
+        if( platyPosition2D.distanceTo( targetPosition2D ) > 0.1)
         {        
             
-            
-            targetDirection.copy( targetPosition );
-            targetDirection.sub( platy.position );
-            
-            
-            
+            // Interprete heading in 2D
+            targetDirection.x = targetPosition.x - platy.position.x;
+            targetDirection.y = 0;
+            targetDirection.z = targetPosition.z - platy.position.z;                        
             targetDirection.normalize();
-            
-            platy.rotation.y = Math.atan(targetDirection.z / targetDirection.x)
-            
-            if(targetDirection.z < 0) {
-                platy.rotation.y += 3.14;
+
+            // Set orientation of our platy
+            platy.rotation.y = Math.atan(targetDirection.x / targetDirection.z)
+
+            if(targetDirection.z < 0)             
+            {
+              platy.rotation.y = platy.rotation.y - Math.PI;
             }
             
-            targetDirection.multiplyScalar(0.1);
+            // Platy walks around
+            targetDirection.multiplyScalar(0.1);                        
+            platy.position.x += targetDirection.x;
+            platy.position.z += targetDirection.z;
             
-     
-            platy.position.add( targetDirection );    
-            
-        }
-            
-	};
+            // Find ground point on surface by casting ray from sky to surface
+            raycaster.ray.origin    = new THREE.Vector3( platy.position.x, 100, platy.position.z )  ;       
+            raycaster.ray.direction = new THREE.Vector3( 0, -1, 0 );    // Down vector
+
+            var intersects = raycaster.intersectObject( surface );
+
+            for ( var i = 0; i < intersects.length; i++ ) 
+            {                    
+                platy.position.y = intersects[ i ].point.y + 0.8;   // Hack to lift platy a little bit up        
+            }            
+        }            
+    };
 
 
     this.mouseUp = function(mouseX, mouseY) {
@@ -162,21 +167,19 @@ function GameScreen() {
     
     
     this.mouseDown = function(mouseX, mouseY) {
+        // Picking position on surface  
         var mouse = new THREE.Vector2(mouseX, mouseY);
         
         raycaster.setFromCamera( mouse, camera );
         
-        // calculate objects intersecting the picking ray
-        var intersects = raycaster.intersectObjects( scene.children );
+        var intersects = raycaster.intersectObject( surface );
 
-        for ( var i = 0; i < intersects.length; i++ ) {    
-
-            targetPosition.copy( intersects[ i ].point );
-            //console.log(targetPosition)
-            targetPosition.y += 1;
-        }
+        for ( var i = 0; i < intersects.length; i++ ) 
+        {    
+            //console.log("Hit: ", intersects[ i ].point)
         
-        
+            targetPosition.copy( intersects[ i ].point );        
+        }       
     }
        
 }
